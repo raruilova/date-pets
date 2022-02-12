1ace1dd29755349a7a0b1f26afd045432024231d
robinson <robinjosellove@gmail.com>
2022-02-12 12:17:27 -0500

    visual aspect

--- src/components/PetForm.jsx
+++ src/components/PetForm.jsx
@@ -28,7 +28,7 @@ const PetForm = () => {
         />
         <label htmlFor="nameOwner" className="font-bold">Nombre dueño</label>
         <input type="text" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Nombre del dueño" name="namOwner"  required/>
-        <label htmlFor="email">Emial</label>
+        <label htmlFor="email" className="font-bold">Email</label>
         <input type="email" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Email" name="email" />
         <label htmlFor="symptoms" className="font-bold">Sintomas</label>
         <textarea
